1b773f8be5c278b554d88709ccdd573ec65d3362
Tom Choi <jw95@live.cn>
2023-01-18 08:00:36 -0500

    Player Movement ・　プレーヤー　操作

--- happiness-shiawase/Assets/Scripts/Player.cs
+++ happiness-shiawase/Assets/Scripts/Player.cs
@@ -6,12 +6,34 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private int health;
 
+
+    [SerializeField] private float speed = 5.0f;
+    [SerializeField] private float jumpPower = 10.0f;
+
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask wallLayer;
+    private Rigidbody2D body;
+    //private Animator animator;
+    private BoxCollider2D boxCollider;
+
+    private float wallJumpCD;
+
+
+    private float horizontalInput;
+
     public int Health
     {
         get { return health; }
         set { health = value; }
     }
 
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        //animator = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -20,8 +42,94 @@ public class Player : MonoBehaviour
     }
 
     // Update is called once per frame
+    // Update is called once per frame
     void Update()
     {
-        
+
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        if (horizontalInput > 0.01f)
+        {
+            transform.localScale = Vector3.one;
+
+        }
+        else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+
+
+        //animator.SetBool("run", horizontalInput != 0);
+        //animator.SetBool("grounded", isGrounded());
+
+        if (wallJumpCD > 0.2f)
+        {
+
+            body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+            if (onWall() && !isGrounded())
+            {
+                body.gravityScale = 0;
+                body.velocity = Vector2.zero;
+            }
+            else
+            {
+                body.gravityScale = 2.5f;
+                ;
+            }
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Jump();
+            }
+        }
+        else
+        {
+            wallJumpCD += Time.deltaTime;
+        }
+    }
+
+    private void Jump()
+    {
+        if (isGrounded())
+        {
+            body.velocity = new Vector2(body.velocity.x, jumpPower);
+            //animator.SetTrigger("jump");
+        }
+        else if (onWall() && !isGrounded())
+        {
+
+            if (horizontalInput == 0)
+            {
+                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 10, 0);
+                transform.localScale = new Vector3(-Mathf.Sign(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            }
+            else
+            {
+                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 3, 6);
+            }
+            wallJumpCD = 0;
+        }
+
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+        }
+    }
+
+    private bool isGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
+    }
+
+    private bool onWall()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0,
+            new Vector2(transform.localScale.x, 0), 0.1f, wallLayer);
+        return raycastHit.collider != null;
     }
 }
